0cdd5f80ea0fdefa3fbc83d9978173d73265923b
Ivan-R-BS <103216417+Ivan-R-BS@users.noreply.github.com>
2022-05-11 18:44:10 -0700

    updated
    up

--- Add.java
+++ Add.java
@@ -1,3 +1,6 @@
 public class Add {
-
+    public static int adding(int a ,int b)
+    {
+        return a+b;
+    }
 }
---
+++ AddTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class AddTest {
+    @Test
+    public void add1{
+        assertEquals(1,Add.adding(1,1));
+    }
+}
